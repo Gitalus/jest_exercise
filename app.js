a5d490795a61074ce83fc9c9ea9b4e329f34954c
Gitalus <italog.penaloza@gmail.com>
2021-08-17 15:27:19 +0000

    add the 3 functions necessary to the exercise

--- app.js
+++ app.js
@@ -5,3 +5,20 @@ function sum(a, b) {
 console.log(sum(7, 3));
 
 module.exports = { sum };
+
+let oneEuroIs = {
+    "JPY": 127.9,
+    "USD": 1.2,
+    "GBP": 0.8
+}
+
+function fromDollarToYen (dollars) {
+    return dollars * oneEuroIs.JPY;
+}
+
+function fromEuroToDollar (euros) {
+    return euros * oneEuroIs.USD;
+}
+function fromYenToPound (yens) {
+    return yens * oneEuroIs.GBP;
+}
